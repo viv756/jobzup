6148edad5f0e8ef366945f59af1d804287cc30a9
*vivek <vrvivekofficial@gmail.com>
2025-07-18 13:32:56 +0530

    connected to mongodb

---
+++ server/src/config/app.config.ts
@@ -0,0 +1,10 @@
+import { getEnv } from "../utils/get-env";
+
+const appConfig = () => ({
+  NODE_ENV: getEnv("NODE_ENV", "development"),
+  PORT: getEnv("PORT", "5000"),
+  BASE_PATH: getEnv("BASE_PATH", "/api"),
+  MONGO_URI: getEnv("MONGO_URI", ""),
+});
+
+export const config = appConfig();
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,12 @@
+import "dotenv/config";
 import express from "express";
+
+import { config } from "./config/app.config";
+import connectDatabase from "./config/database.config";
 
 const app = express();
 
-app.listen("8000", () => {
-  console.log("server is running");
+app.listen(config.PORT, async () => {
+  console.log(`Server listening on port ${config.PORT} in ${config.NODE_ENV}`);
+  await connectDatabase();
 });

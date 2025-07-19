b1fa64897529932e7b857dae8f32bc38f71789da
*vivek <vrvivekofficial@gmail.com>
2025-07-19 12:01:50 +0530

    created custom error handlers

---
+++ server/src/utils/appError.ts
@@ -0,0 +1,24 @@
+import { HTTPSTATUS, HttpStatusCodeType } from "../config/http.config";
+import { ErrorCodeEnum, ErrorCodeEnumType } from "../enums/error-code.enum";
+
+export class AppError extends Error {
+  public statusCode: HttpStatusCodeType;
+  public errorCode?: ErrorCodeEnumType;
+
+  constructor(
+    message: string,
+    statusCode = HTTPSTATUS.INTERNAL_SERVER_ERROR,
+    errorCode?: ErrorCodeEnumType
+  ) {
+    super(message);
+    this.statusCode = statusCode;
+    this.errorCode = errorCode;
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+export class NOTFoundExeption extends AppError {
+  constructor(message = "Resource not found", errorCode?: ErrorCodeEnumType) {
+    super(message, HTTPSTATUS.NOT_FOUND, errorCode || ErrorCodeEnum.RESOURCE_NOT_FOUND);
+  }
+}

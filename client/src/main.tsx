19a7d90fbba6fa297dd750badf3e7e387844b018
*vivek <vrvivekofficial@gmail.com>
2025-09-04 22:56:47 +0530

    *

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,8 +6,8 @@ import "./index.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
+  <>
     <App />
     <Toaster />
-  </StrictMode>
+  </>
 );

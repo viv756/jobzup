3e9138f6c99d2b4db1b235e5d61921aaeddf9706
*vivek <vrvivekofficial@gmail.com>
2025-09-04 23:12:02 +0530

    *

--- client/src/pages/profile/recruiter/my-jobs/_components/JobsTable.tsx
+++ client/src/pages/profile/recruiter/my-jobs/_components/JobsTable.tsx
@@ -86,7 +86,7 @@ const JobsTable = () => {
                   <input
                     id="checkbox-all-search"
                     type="checkbox"
-                    className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 focus:ring-2 "
+                    className="w-4 h-4 text-blue-600 accent-blue-700 bg-blue-700 border-gray-300 rounded-sm focus:ring-blue-500 focus:ring-2 "
                   />
                   <label htmlFor="checkbox-all-search" className="sr-only">
                     checkbox
@@ -122,7 +122,7 @@ const JobsTable = () => {
                       <input
                         id="checkbox-table-search-1"
                         type="checkbox"
-                        className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500  focus:ring-2 "
+                        className="w-4 h-4 text-blue-600 bg-gray-100 accent-blue-700 border-gray-300 rounded-sm focus:ring-blue-500  focus:ring-2 "
                       />
                       <label htmlFor="checkbox-table-search-1" className="sr-only">
                         checkbox

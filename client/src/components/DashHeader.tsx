4ae8e4af2ce9edd905a3a9c5cb16a8d5950cf968
*vivek <vrvivekofficial@gmail.com>
2025-09-13 11:24:39 +0530

    created a dash header

---
+++ client/src/components/DashHeader.tsx
@@ -0,0 +1,38 @@
+import { Link } from "react-router-dom";
+import { useAppSelector } from "../hooks/useSelector";
+
+const DashHeader = () => {
+  const { currentUser } = useAppSelector((store) => store.user);
+
+  const capitalizeFirstLetter = () => {
+    if (currentUser?.role) {
+      const role = currentUser.role.toLowerCase();
+      return role.charAt(0).toUpperCase() + role.slice(1);
+    }
+  };
+
+  return (
+    <>
+      {currentUser && (
+        <div className="bg-white h-20 flex items-center justify-between">
+          <Link to={"/"} className="font-dm hover:text-blue-700">
+            Back to home
+          </Link>
+          <div className="flex gap-3 items-center ">
+            <img
+              src={currentUser?.profilePicture}
+              alt=""
+              className="w-13 h-13 rounded-full object-cover"
+            />
+            <div>
+              <p className="font-dm"> {currentUser?.name}</p>
+              <p className="font-dm text-xs">{capitalizeFirstLetter()}</p>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default DashHeader;
--- client/src/layout/AppLayout.tsx
+++ client/src/layout/AppLayout.tsx
@@ -1,11 +1,15 @@
 import { Outlet } from "react-router-dom";
 
 import SideBar from "../components/SideBar";
+import DashHeader from "../components/DashHeader";
 
 const AppLayout = () => {
   return (
     <>
       <SideBar />
+      <div className="pl-[310px] pr-[30px]">
+        <DashHeader />
+      </div>
       <div className="bg-[#F2F4F7] pl-[310px] pr-[30px]">
         <Outlet />
       </div>

5b60543ca87fe22e5d3f2117af848626d0d0196e
*vivek <vrvivekofficial@gmail.com>
2025-09-15 14:42:17 +0530

    create reusable modal

---
+++ client/src/components/Modal.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+type ModalProps = {
+  isOpen: boolean;
+  onClose: () => void;
+  title?: string;
+  children: React.ReactNode;
+};
+
+const Modal: React.FC<ModalProps> = ({ isOpen, onClose, title, children }) => {
+  return (
+    <dialog className="modal " open={isOpen}>
+      <div className="modal-box">
+        <form method="dialog" className="absolute right-2 top-2">
+          <button type="button" className="btn btn-sm btn-circle btn-ghost" onClick={onClose}>
+            ✕
+          </button>
+        </form>
+
+        {title && <h3 className="font-bold text-lg">{title}</h3>}
+        <div className="py-4 ">{children}</div>
+      </div>
+
+      {/* overlay to close when clicking outside */}
+    </dialog>
+  );
+};
+
+export default Modal;

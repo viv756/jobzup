b1247274d5abafe79107a08c25e72f3a8b3f0846
*vivek <vrvivekofficial@gmail.com>
2025-09-24 17:02:12 +0530

    hero section created

---
+++ client/src/pages/home/_components/HeroSection.tsx
@@ -0,0 +1,49 @@
+const HeroSection = () => {
+  return (
+    <div className=" relative flex justify-center mt-10">
+      <img
+        src="/icon1.svg"
+        alt=""
+        className="-z-10 w-16 absolute left-3 top-17 animate-zoom-in-out"
+      />
+      <img
+        src="/icon2.svg"
+        alt=""
+        className="-z-10 w-10 absolute left-3 bottom-60 animate-zoom-in-out"
+      />
+      <img
+        src="/icon3.svg"
+        alt=""
+        className="-z-10 w-10 absolute left-80 top-50 animate-zoom-in-out"
+      />
+      <img
+        src="/icon4.svg"
+        alt=""
+        className="-z-10 w-16 absolute left-40 bottom-10 animate-zoom-in-out"
+      />
+      <img src="/home-girl.png" alt="" className="object-cover z-10" />
+      <img
+        src="/icon5.svg"
+        alt=""
+        className="-z-10 w-16 absolute right-3 top-17 animate-zoom-in-out"
+      />
+      <img
+        src="/icon6.svg"
+        alt=""
+        className="-z-10 w-10 absolute right-80 top-50 animate-zoom-in-out"
+      />
+      <img
+        src="/icon7.svg"
+        alt=""
+        className="-z-10 w-16 absolute right-30 bottom-60 animate-zoom-in-out"
+      />
+      <img
+        src="/icon8.svg"
+        alt=""
+        className="-z-10 w-16 absolute right-70 bottom-10 animate-zoom-in-out"
+      />
+    </div>
+  );
+};
+
+export default HeroSection;
